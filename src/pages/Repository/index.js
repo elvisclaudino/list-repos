98023f92f7b3f991347d3bfaa04000373668aabc
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2025-02-24 17:36:06 -0300

    feat: add issues information

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -5,6 +5,7 @@ import { Container } from "./styles";
 import { Owner } from "./styles";
 import { Loading } from "./styles";
 import { BackButton } from "./styles";
+import { IssuesList } from "./styles";
 
 import api from "../../services/api";
 import { FaArrowLeft } from "react-icons/fa";
@@ -54,6 +55,25 @@ export default function Repository({ match }) {
         <h1>{repositoryData.name}</h1>
         <p>{repositoryData.description}</p>
       </Owner>
+
+      <IssuesList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
     </Container>
   );
 }
